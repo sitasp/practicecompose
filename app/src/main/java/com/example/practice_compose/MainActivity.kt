60cf547463b78614585556fe1237713f04133515
satish.patra <psatishkumar720@gmail.com>
2025-05-22 21:27:32 +0530

    modifier - circle shape

--- app/src/main/java/com/example/practice_compose/MainActivity.kt
+++ app/src/main/java/com/example/practice_compose/MainActivity.kt
@@ -7,10 +7,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.painterResource
@@ -31,6 +33,7 @@ class MainActivity : ComponentActivity() {
                 Box(
                     modifier = Modifier
                         .size(100.dp)
+                        .clip(CircleShape)
                         .background(Color.Green)
                 )
             }
