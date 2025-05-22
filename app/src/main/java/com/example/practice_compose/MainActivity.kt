f25c49cdd12035b3c6023c6ef1139725032489ab
satish.patra <psatishkumar720@gmail.com>
2025-05-22 21:08:52 +0530

    modifier added
    > visual property
    > add bg color
    > add click property

--- app/src/main/java/com/example/practice_compose/MainActivity.kt
+++ app/src/main/java/com/example/practice_compose/MainActivity.kt
@@ -5,15 +5,18 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.practice_compose.ui.theme.PracticecomposeTheme
 
@@ -21,14 +24,12 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Row(
-                modifier = Modifier.fillMaxSize(),
-                horizontalArrangement = Arrangement.SpaceAround,
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Text("Hello")
-                Text("World!")
-                Text("It's me Satish!!")
+            Box(
+                modifier = Modifier
+                    .size(100.dp)
+                    .background(color=Color.Red, shape= RectangleShape)
+            ){
+                
             }
         }
     }
