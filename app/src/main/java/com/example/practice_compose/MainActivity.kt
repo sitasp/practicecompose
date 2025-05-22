40121ac009ca72e1c538c110dce81dddbe782079
satish.patra <psatishkumar720@gmail.com>
2025-05-23 02:40:35 +0530

    states - stateful compose added

--- app/src/main/java/com/example/practice_compose/MainActivity.kt
+++ app/src/main/java/com/example/practice_compose/MainActivity.kt
@@ -1,50 +1,40 @@
 package com.example.practice_compose
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.example.practice_compose.ui.theme.PracticecomposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-
-                // creating states
-                var textState by remember {
-                    mutableStateOf("")
-                };
-
-                TextField(
-                    value = textState,
-                    onValueChange = {
-                        textState = it
-                    },
-                    modifier = Modifier.fillMaxWidth()
-                )
+                MyTextField()
             }
         }
     }
+}
+
+
+
+// stateful component
+@Composable
+fun MyTextField () {
+    // creating states
+    var textState by remember {
+        mutableStateOf("")
+    };
+
+    TextField(
+        value = textState,
+        onValueChange = {
+            textState = it
+        },
+        modifier = Modifier.fillMaxWidth()
+    )
 }
