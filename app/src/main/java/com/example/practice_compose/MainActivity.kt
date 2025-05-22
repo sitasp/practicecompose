fcc717a0b9d4a68aa1797353408c924da2d6ddbf
satish.patra <psatishkumar720@gmail.com>
2025-05-23 02:59:54 +0530

    states - list of names string added

--- app/src/main/java/com/example/practice_compose/MainActivity.kt
+++ app/src/main/java/com/example/practice_compose/MainActivity.kt
@@ -3,7 +3,11 @@ package com.example.practice_compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -13,14 +17,35 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+            Column(modifier = Modifier.fillMaxSize()) {
+
                 var textState by remember {
                     mutableStateOf("")
                 }
+
+                val namesListState = remember {
+                    mutableStateListOf<String>()
+                }
+
+                LazyColumn(
+                    modifier = Modifier.fillMaxWidth()
+                        .weight(1f)
+                ){
+                    items(namesListState.size) { index ->
+                        Text(
+                            text = namesListState[index]
+                        )
+                    }
+                }
+
                 MyTextField(
                     textState = textState,
                     onValueChanged = {
                         textState = it
+                    },
+                    onAddClick = {
+                        namesListState.add(textState)
+                        textState = ""
                     }
                 )
             }
@@ -34,13 +59,17 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MyTextField (
     textState: String,
-    onValueChanged: (String) -> Unit
+    onValueChanged: (String) -> Unit,
+    onAddClick: () -> Unit
 ) {
     TextField(
         value = textState,
         onValueChange = {
             onValueChanged(it)
         },
-        modifier = Modifier.fillMaxWidth()
+        modifier = Modifier.fillMaxWidth(),
+        trailingIcon = {
+            Icon(imageVector = Icons.Default.Add, contentDescription = null, modifier = Modifier.clickable { onAddClick() })
+        }
     )
 }
