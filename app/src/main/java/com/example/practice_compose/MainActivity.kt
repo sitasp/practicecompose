362ffd4c9ab5a5bd0cf95040c3fd622958b164f6
satish.patra <psatishkumar720@gmail.com>
2025-05-22 20:36:05 +0530

    box modifier
    hello and welcome overlapping on each other

--- app/src/main/java/com/example/practice_compose/MainActivity.kt
+++ app/src/main/java/com/example/practice_compose/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.*
@@ -21,36 +22,10 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Greeting("Satish")
-            Button(onClick = { println("button clicked") }, content = {
-                Text(text = "Click Me")
-            })
-//            Icon(painter = painterResource(id = R.drawable.ic_launcher_foreground), contentDescription = "icon", tint = Color.Black)
-//            Image(
-//                painter = painterResource(id = R.drawable.ic_launcher_foreground),
-//                contentDescription = "icon"
-//            )
-//            TextField(value = "", onValueChange = {}, label = { Text(text = "Enter your name") }, placeholder = { Text(text = "Name") }, colors = TextFieldDefaults.colors(
-//                focusedIndicatorColor = Color.Red,
-//                unfocusedIndicatorColor = Color.Blue,
-//                focusedContainerColor = Color.Yellow,
-//                unfocusedContainerColor = Color.Green
-//            ))
-//            FloatingActionButton(onClick = {}) {
-//                Icon(painter = painterResource(id = R.drawable.ic_launcher_foreground), contentDescription = "icon")
-//            }
-//            ExtendedFloatingActionButton(
-//                text = { Text(text = "Extended FAB") },
-//                icon = { Icon(painter = painterResource(id = R.drawable.ic_launcher_foreground), contentDescription = "icon") },
-//                onClick = { println("Extended FAB clicked") },
-//                containerColor = Color.White,
-//            )
+            Box(modifier = Modifier.fillMaxSize()) {
+                Text(text = "Hello")
+                Text(text = "Welcome")
+            }
         }
     }
-}
-
-
-@Composable
-fun Greeting(name: String) {
-    Text("Welcome $name", color = Color.Red, fontSize = 22.sp, fontWeight = FontWeight.Bold)
 }
