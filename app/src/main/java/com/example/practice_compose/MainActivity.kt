404b6019b5f0c5b699df67aa1b8289f326c3e45c
satish.patra <psatishkumar720@gmail.com>
2025-05-23 20:20:38 +0530

    button - material3 google sample elevated button example
    > download the material catalog api from google
    > see mui2 and mui3 (mui = material ui)
    > used elevatedbutton example
    > can use any button

--- app/src/main/java/com/example/practice_compose/MainActivity.kt
+++ app/src/main/java/com/example/practice_compose/MainActivity.kt
@@ -3,25 +3,10 @@ package com.example.practice_compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
-import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.AnnotatedString
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.withStyle
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
 
@@ -32,13 +17,10 @@ class MainActivity : ComponentActivity() {
                 modifier = Modifier.fillMaxSize(),
                 contentAlignment = Alignment.Center
             ) {
-                IconButton(onClick = {
-                    println("Button clicked")
-                }) {
-                    Icon(
-                        painter = painterResource(R.drawable.ic_launcher_foreground),
-                        contentDescription = "Icon",
-                    )
+                ElevatedButton(
+                    onClick = {}
+                ) {
+                    Text("Click me!!")
                 }
             }
         }
