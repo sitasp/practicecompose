3532a4ca3adb44fff77c7549dc90f7ac0a165dff
satish.patra <psatishkumar720@gmail.com>
2025-05-23 14:06:00 +0530

    text - use of string resources, xml file config
    > string resources usage
    > maxlines, overflow use TextOverFlow.Ellipsis

--- app/src/main/java/com/example/practice_compose/MainActivity.kt
+++ app/src/main/java/com/example/practice_compose/MainActivity.kt
@@ -10,68 +10,36 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : ComponentActivity() {
-
-    val viewModel by lazy {
-        ViewModelProvider(this).get(MyViewModel::class.java)
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
 
-            val state = viewModel.state.value
+            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
 
-            Column(modifier = Modifier.fillMaxSize()) {
-
-                LazyColumn(
-                    modifier = Modifier.fillMaxWidth()
-                        .weight(1f)
-                ){
-                    items(state.namesList.size) { index ->
-                        Text(
-                            text = state.namesList[index]
-                        )
-                    }
-                }
-
-                MyTextField(
-                    textState = state.text,
-                    onValueChanged = {
-                        viewModel.updateText(it)
-                    },
-                    onAddClick = {
-                        viewModel.updateNamesList(state.text)
-                        viewModel.updateText("")
-                    }
+                Text(
+                    text = stringResource(id = R.string.land_of_sages)
+                                    .plus(" ")
+                                    .repeat(30),
+                    maxLines = 3,
+                    overflow = TextOverflow.Ellipsis
                 )
+
+
+
+//                Text(
+//                    text = "Land of sages, aryavarta",
+//                    color =
+//                )
+
             }
         }
     }
-}
-
-
-
-// stateless component
-@Composable
-fun MyTextField (
-    textState: String,
-    onValueChanged: (String) -> Unit,
-    onAddClick: () -> Unit
-) {
-    TextField(
-        value = textState,
-        onValueChange = {
-            onValueChanged(it)
-        },
-        modifier = Modifier.fillMaxWidth(),
-        trailingIcon = {
-            Icon(imageVector = Icons.Default.Add, contentDescription = null, modifier = Modifier.clickable { onAddClick() })
-        }
-    )
 }
