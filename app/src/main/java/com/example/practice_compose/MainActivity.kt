b4dd90d807bc368a2b9000b2cd361606513622de
satish.patra <psatishkumar720@gmail.com>
2025-05-27 21:21:05 +0530

    coroutine added

--- app/src/main/java/com/example/practice_compose/MainActivity.kt
+++ app/src/main/java/com/example/practice_compose/MainActivity.kt
@@ -8,18 +8,24 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.practice_compose.screens.QuoteListScreen
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        DataManager.loadAssetsFromFile(this)
+        CoroutineScope(Dispatchers.IO).launch {
+//            delay(10000) -- testing purpose
+            DataManager.loadAssetsFromFile(applicationContext)
+        }
 
         setContent {
             App()
-
         }
     }
 }
@@ -29,5 +35,17 @@ class MainActivity : ComponentActivity() {
 fun App() {
     if(DataManager.isDataLoaded.value) {
         QuoteListScreen(data = DataManager.data) { }
+    }
+    else {
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier.fillMaxSize(1f)
+        ) {
+            Text(
+                text = "Loading...",
+                style = MaterialTheme.typography.headlineSmall,
+                modifier = Modifier.padding(16.dp)
+            )
+        }
     }
 }
